77f63e171b39c7a51fb6d86539ccb3f2993b2234
MinhSang97 <minhsangnguyen463@gmail.com>
2024-05-26 19:48:24 +0700

    Update

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/MinhSang97/order_app/redis"
+	"github.com/MinhSang97/order_app/router"
+)
+
+func main() {
+	router.Route()
+	redis.ConnectRedis()
+	//middleware.SaveLogRequest()
+}
