ba74958949c5941f1764fdde6696ee137214c192
MinhSang97 <minhsangnguyen463@gmail.com>
2024-05-27 11:56:40 +0700

    Update

--- router/route.go
+++ router/route.go
@@ -3,6 +3,7 @@ package router
 import (
 	"fmt"
 	"github.com/MinhSang97/order_app/dbutil"
+	admin_function_member "github.com/MinhSang97/order_app/handler/admin_function/member"
 	admin_login "github.com/MinhSang97/order_app/handler/admin_login"
 	users_login "github.com/MinhSang97/order_app/handler/users_login"
 	"github.com/MinhSang97/order_app/middleware"
@@ -13,17 +14,6 @@ import (
 func Route() {
 	db := dbutil.ConnectDB()
 	fmt.Println("Connected: ", db)
-
-	// CRUD: Create, Read, Update, Delete
-	// POST /v1/items (create a new item)
-	// GET /v1/items (list items) /v1/items?page=1
-	// GET /v1/items/:id (get item detail by id)
-	// (PUT | PATCH) /v1/items/:id (update an item by id)
-	// DELETE /v1/items/:id (delete item by id)
-	//viper.SetConfigFile("config.yaml")
-	//if err := viper.ReadInConfig(); err != nil {
-	//	panic(err)
-	//}
 
 	r := gin.Default()
 	r.Use(middleware.ErrorHandler())
@@ -44,8 +34,11 @@ func Route() {
 			api.PATCH("/admin/update/:user_id", middleware.JWTMiddlewareAdmin(), admin_login.AdminUpdate())
 			api.DELETE("/admin/delete/:user_id", middleware.JWTMiddlewareAdmin(), admin_login.AdminDelete())
 
+			//admin_function_member
+			api.GET("/admin/member-view", middleware.JWTMiddlewareAdmin(), admin_function_member.AdminMemberView())
+
 			//user
-			api.POST("/users/sign-up", middleware.JWTMiddlewareAdmin(), users_login.UsersSignUp())
+			api.POST("/users/sign-up", users_login.UsersSignUp())
 			api.GET("/users/sign-in", users_login.UsersSignIn())
 			api.PATCH("/users/update/:user_id", middleware.JWTMiddlewareUsers(), users_login.UsersUpdate())
 			api.DELETE("/users/delete/:user_id", middleware.JWTMiddlewareAdmin(), users_login.UsersDelete())
