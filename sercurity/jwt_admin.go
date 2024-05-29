a326c9977a2f1cb19b14bf743a2bf0be5b2558ef
MinhSang97 <minhsangnguyen463@gmail.com>
2024-05-29 16:41:53 +0700

    Update Jwt admin

--- sercurity/jwt_admin.go
+++ sercurity/jwt_admin.go
@@ -1,41 +1,3 @@
-//package sercurity
-//
-//import (
-//	"fmt"
-//	"github.com/MinhSang97/order_app/usecases/dto/admin_dto"
-//
-//	"github.com/golang-jwt/jwt"
-//	"time"
-//)
-//
-//const SECRET_KEY_ADMIN = "learngolanglalalafdfds"
-//
-//type JwtCustomClaims struct {
-//	UserId string
-//	Role   string
-//	jwt.StandardClaims
-//}
-//
-//func GenTokenAdmin(user admin_dto.Admin) (string, error) {
-//	claims := &JwtCustomClaims{
-//		UserId: user.UserId,
-//		Role:   user.Role,
-//		StandardClaims: jwt.StandardClaims{
-//			ExpiresAt: time.Now().Add(time.Hour * 3).Unix(),
-//		},
-//	}
-//
-//	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-//	result, err := token.SignedString([]byte(SECRET_KEY_ADMIN))
-//	if err != nil {
-//		fmt.Println("Loi tao token", err.Error())
-//		return "Tao Token Loi!", err
-//	}
-//
-//	return result, nil
-//
-//}
-
 package sercurity
 
 import (
@@ -70,6 +32,7 @@ func GenTokenAdmin(user admin_dto.Admin) (string, error) {
 		return existingToken, nil
 	}
 
+	//Payload
 	claims := &JwtCustomClaims{
 		UserId: user.UserId,
 		Role:   user.Role,
@@ -77,8 +40,10 @@ func GenTokenAdmin(user admin_dto.Admin) (string, error) {
 			ExpiresAt: time.Now().Add(time.Hour * 8).Unix(),
 		},
 	}
-
+	//Header
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	//Signature
 	result, err := token.SignedString([]byte(SECRET_KEY_ADMIN))
 	if err != nil {
 		fmt.Println("Lỗi tạo token:", err.Error())
