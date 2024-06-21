ac29e9be95260db3957f8b1cf3837ae16afad082
MinhSang97 <minhsangnguyen463@gmail.com>
2024-06-21 22:42:38 +0700

    Update Dockerfile

--- run_migrations.sh
+++ run_migrations.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Wait for the PostgreSQL database to be ready
-until pg_isready -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER}; do
-  echo "Waiting for PostgreSQL to be ready..."
-  sleep 5
-done
+## Wait for the PostgreSQL database to be ready
+#until pg_isready -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER}; do
+#  echo "Waiting for PostgreSQL to be ready..."
+#  sleep 5
+#done
 
 
 # Navigate to the db folder and run the migrations
