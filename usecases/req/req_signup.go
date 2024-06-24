538d2b788a5919622add44116d01b5cf7f78974d
MinhSang97 <minhsangnguyen463@gmail.com>
2024-06-24 23:32:50 +0700

    Update

--- usecases/req/req_signup.go
+++ usecases/req/req_signup.go
@@ -1,7 +1,7 @@
 package req
 
 type ReqSignUp struct {
-	Email        string  `json:"email,omitempty" validate:"required"`
+	Email        string  `json:"email,omitempty" validate:"required,email"`
 	PassWord     string  `json:"password,omitempty" validate:"required"`
 	Name         string  `json:"name,omitempty" validate:"required"`
 	PhoneNumber  string  `json:"phone_number,omitempty" validate:"required"`
